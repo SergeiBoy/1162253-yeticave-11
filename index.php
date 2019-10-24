ebc9dd505b7bd2c212d27b013eea1ae0f712ef6a
SergeiBoy <s_shchetinin@mail.ru>
2019-10-24 19:58:36 +0500

    Поправил на === и добавил ; после

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ $user_name = 'Сергей'; // укажите здесь ваше имя
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 		
-<?php if ($is_auth == 1): ?>
+<?php if ($is_auth === 1): ?>
 		<div class="user-menu__logged">
-			<p><?=$user_name ?></p>
+			<p><?=$user_name; ?></p>
 			<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
 			<a class="user-menu__logout" href="#">Выход</a>
 		</div>
