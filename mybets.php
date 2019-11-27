f6ec494a9e8d003a45fe1423ed3015f417b66f1a
SergeiBoy <s_shchetinin@mail.ru>
2019-11-27 14:26:03 +0500

    Доделали страницу мои ставки

--- mybets.php
+++ mybets.php
@@ -11,13 +11,13 @@ require_once('startup.php'); //Подключение к БД и получен�
 //Получаем список ставок
 $cur_user_id = $_SESSION['user']['id'];
 $sql = "SELECT lots.id, lot_name, img_path, category_name, MAX(bid_price) AS bid_price, 
-MAX(bids.dt_add) AS dt_add, MAX(DATE_FORMAT(bids.dt_add, '%d.%m.%y %H:%i')) AS dt_add_format, dt_end, email, contact_info
+MAX(bids.dt_add) AS dt_add, MAX(DATE_FORMAT(bids.dt_add, '%d.%m.%y %H:%i')) AS dt_add_format, dt_end, email, contact_info, user_id_winner
 FROM bids LEFT JOIN lots ON bids.lot_id = lots.id
 LEFT JOIN categories ON lots.category_id = categories.id
 LEFT JOIN users ON lots.user_id_author = users.id
 WHERE bids.user_id = $cur_user_id
 GROUP BY lots.id, lot_name, img_path, category_name, dt_end, email, contact_info
-ORDER BY dt_end ASC";
+ORDER BY dt_end DESC";
 $result = mysqli_query($con, $sql);
 	if (!$result) {
 	$error = mysqli_error($con);
@@ -25,6 +25,16 @@ $result = mysqli_query($con, $sql);
 	} 
 $goods = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+foreach ($goods as &$good) {
+	$good['is_win'] = false;
+	$good['is_ended'] = false;
+	if ( $_SESSION['user']['id'] === intval($good['user_id_winner']) ) {
+		$good['is_win'] = true;
+	}
+	if ( strtotime($good['dt_end']) <=  time() ) {
+		$good['is_ended'] = true;
+	}
+}
 
 $page_content = include_template('my-bets.php', 
 ['categories' => $categories, 'goods' => $goods]);
--- templates/my-bets.php
+++ templates/my-bets.php
@@ -11,19 +11,44 @@
       <h2>Мои ставки</h2>
       <table class="rates__list">
 		<?php foreach ($goods as $good): ?>
-		<tr class="rates__item">
+		<tr class="rates__item  <?php if ( $good['is_win'] ): ?>
+								rates__item--win
+								<?php elseif ( $good['is_ended'] ) : ?>
+								rates__item--end
+								<?php else: ?>
+								''
+								<?php endif; ?>     ">
           <td class="rates__info">
             <div class="rates__img">
               <img src="<?=htmlspecialchars($good['img_path']);?>" width="54" height="40" alt="<?=htmlspecialchars($good['lot_name']);?>">
             </div>
+			<div>
             <h3 class="rates__title"><a href="lot.php?id=<?=$good['id']; ?>"><?=htmlspecialchars($good['lot_name']);?></a></h3>
+			<?php if ( $good['is_win'] ): ?>
+			<p><?=$good['contact_info'].' '.$good['email']; ?></p>
+			<?php endif; ?>
+			</div>
           </td>
           <td class="rates__category">
             <?=($good['category_name']);?>
           </td>
           <td class="rates__timer">
-            <div class="timer <?php if ( get_time_remaining($good['dt_end'])[0] < 1 ): ?>timer--finishing<?php endif; ?>">
+            <div class="timer 	<?php if ( $good['is_win'] ): ?>
+								timer--win
+								<?php elseif ( $good['is_ended'] ) : ?>
+								timer--end
+								<?php elseif ( get_time_remaining($good['dt_end'])[0] < 1 ) : ?>
+								timer--finishing
+								<?php else: ?>
+								''
+								<?php endif; ?>     ">
+			<?php if ( $good['is_win'] ): ?>
+			Ставка выиграла
+			<?php elseif ( $good['is_ended'] ) : ?>
+			Торги окончены
+			<?php else: ?>
 			<?=implode(':', get_time_remaining($good['dt_end']) );?>
+			<?php endif; ?>
 			</div>
           </td>
           <td class="rates__price">
